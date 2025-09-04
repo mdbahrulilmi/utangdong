d28c612c595f7db1cd9ba9cd6770f074e07be3df
mdbicode <mdbahrulilmi@gmail.com>
2025-09-04 23:29:12 +0700

    fix(ci) add registrationTest with username

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -12,6 +12,7 @@ test('registration screen can be rendered', function () {
 test('new users can register', function () {
     $response = Livewire::test(Register::class)
         ->set('name', 'Test User')
+        ->set('username', 'testuser123')
         ->set('email', 'test@example.com')
         ->set('password', 'password')
         ->set('password_confirmation', 'password')
