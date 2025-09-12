32a9047713a5e52869e64fb8039896fc811076e9
mdbicode <mdbahrulilmi@gmail.com>
2025-09-13 06:15:29 +0700

    ci(factory) fixed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,9 +29,12 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'borrower',
+            'status' => 'verified', // <- ini penting
             'remember_token' => Str::random(10),
         ];
     }
+
 
     /**
      * Indicate that the model's email address should be unverified.
